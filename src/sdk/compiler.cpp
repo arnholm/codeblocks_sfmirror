ee02ee8465560df18922452cb013cfc9b91c8bc5
wh11204 <wh11204@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2021-12-15 19:57:54 +0000

    * SDK: Allow ! in filenames (ticket #898, part I). Change " \t" to "[:blank:]" in regex
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12584 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/compiler.cpp
+++ src/sdk/compiler.cpp
@@ -36,7 +36,7 @@ wxArrayString Compiler::m_CompilerIDs; // map to guarantee unique IDs
 // common regex that can be used by the different compiler for matching compiler output
 // it can be used in the patterns for warnings, errors, ...
 // NOTE : it is an approximation (for example the ':' can appear anywhere and several times)
-const wxString Compiler::FilePathWithSpaces = _T("[][{}() \t#%$~[:alnum:]&_:+/\\.-]+");
+const wxString Compiler::FilePathWithSpaces = _T("[][{}()[:blank:]#%$~[:alnum:]!&_:+/\\.-]+");
 
 // version of compiler settings
 // when this is different from what is saved in the config, a message appears
