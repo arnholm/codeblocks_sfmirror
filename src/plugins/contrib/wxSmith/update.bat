1ec71f36c9283ebb17f5bc4fc6f02d56bf020444
wh11204 <wh11204@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2022-09-27 12:25:50 +0000

    - wxSmith: Fix update.bat messed in [r12909] (thanks gd_on).
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12912 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/wxSmith/update.bat
+++ src/plugins/contrib/wxSmith/update.bat
@@ -4,10 +4,10 @@ set CB_DEVEL_DIR=devel%1
 set CB_OUTPUT_DIR=output%1
 set CB_DEVEL_RESDIR=%CB_DEVEL_DIR%\share\CodeBlocks
 set CB_OUTPUT_RESDIR=%CB_OUTPUT_DIR%\share\CodeBlocks
-zip ..\..\..\%CB_DEVEL_RESDIR%\wxsmith.zip manifest.xml
-zip ..\..\..\%CB_OUTPUT_RESDIR%\wxsmith.zip manifest.xml
 md  ..\..\..\%CB_DEVEL_RESDIR%\images\wxsmith > nul 2>&1
 md  ..\..\..\%CB_OUTPUT_RESDIR%\images\wxsmith > nul 2>&1
+zip ..\..\..\%CB_DEVEL_RESDIR%\wxsmith.zip manifest.xml
+zip ..\..\..\%CB_OUTPUT_RESDIR%\wxsmith.zip manifest.xml
 copy wxwidgets\icons\*.png ..\..\..\%CB_DEVEL_RESDIR%\images\wxsmith\ > nul 2>&1
 copy wxwidgets\icons\*.png ..\..\..\%CB_OUTPUT_RESDIR%\images\wxsmith\ > nul 2>&1
 copy wxwidgets\icons\*.svg ..\..\..\%CB_DEVEL_RESDIR%\images\wxsmith\ > nul 2>&1
