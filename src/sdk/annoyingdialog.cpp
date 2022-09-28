9c792cf793f558c1a5821a8c522729bcb329d5c0
wh11204 <wh11204@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2022-09-28 06:04:14 +0000

    - Fix size of icon in Annoying Dialog with HiDPI.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12913 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/annoyingdialog.cpp
+++ src/sdk/annoyingdialog.cpp
@@ -111,9 +111,10 @@ void AnnoyingDialog::Init(const wxString &caption, const wxString &id, const wxS
 
     wxFlexGridSizer *mainArea = new wxFlexGridSizer(2, 0, 0);
 #if wxCHECK_VERSION(3, 1, 6)
-    wxStaticBitmap *bitmap = new wxStaticBitmap(this, wxID_ANY, wxArtProvider::GetBitmapBundle(icon, wxART_MESSAGE_BOX));
+    wxStaticBitmap *bitmap = new wxStaticBitmap(this, wxID_ANY, wxArtProvider::GetBitmapBundle(icon, wxART_MESSAGE_BOX, wxSize(32, 32)));
 #else
-    wxStaticBitmap *bitmap = new wxStaticBitmap(this, wxID_ANY, wxArtProvider::GetBitmap(icon, wxART_MESSAGE_BOX));
+    const int height = cbFindMinSize16to64(wxRound(32*cbGetContentScaleFactor(*this)));
+    wxStaticBitmap *bitmap = new wxStaticBitmap(this, wxID_ANY, wxArtProvider::GetBitmap(icon, wxART_MESSAGE_BOX, wxSize(height, height)));
 #endif
     mainArea->Add(bitmap, 0, wxALL, 5);
 
