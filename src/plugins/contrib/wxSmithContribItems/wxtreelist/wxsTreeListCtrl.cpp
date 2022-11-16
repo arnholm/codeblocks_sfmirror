444bf1a97aabca44f0ad59154af4110fda63af27
wh11204 <wh11204@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2022-11-16 09:17:38 +0000

    - wxSmith Contrib: Make some strings not translatable. Code style correction. Removed ; after }.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13038 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/wxSmithContribItems/wxtreelist/wxsTreeListCtrl.cpp
+++ src/plugins/contrib/wxSmithContribItems/wxtreelist/wxsTreeListCtrl.cpp
@@ -37,15 +37,15 @@ namespace
 #include "images/TreeList32.xpm"
 
 wxsRegisterItem<wxsTreeListCtrl> Reg (
-    _T ("wxTreeListCtrl"),          // Class name
+    _T("wxTreeListCtrl"),           // Class name
     wxsTWidget,                     // Item type
-    _T ("wxWindows"),               // License
-    _T ("Ron Collins"),             // Author
-    _T ("rcoll@theriver.com"),      // Author's email
-    _T (""),                        // Item's homepage
-    _T ("Contrib"),                // Category in palette
+    _T("wxWindows"),                // License
+    _T("Ron Collins"),              // Author
+    _T("rcoll@theriver.com"),       // Author's email
+    _T(""),                         // Item's homepage
+    _T("Contrib"),                  // Category in palette
     30,                             // Priority in palette
-    _T ("TreeListCtrl"),            // Base part of names for new items
+    _T("TreeListCtrl"),             // Base part of names for new items
     wxsCPP,                         // List of coding languages supported by this item
     1, 0,                           // Version
     wxBitmap (TreeList32_xpm),      // 32x32 bitmap
@@ -115,39 +115,39 @@ wxsTreeListCtrl::wxsTreeListCtrl (wxsItemResData* Data) :
     m_ColTitles.Clear();
     m_ReadOnly = false;
 
-    m_ColTitles.Add (_ ("!Define Column Titles") );
-    m_ColTitles.Add (_ ("!Text lines that begin with \"!\" are ignored, but blank") );
-    m_ColTitles.Add (_ ("!lines are significant (a blank line is a blank title for") );
-    m_ColTitles.Add (_ ("!a column).") );
-    m_ColTitles.Add (_ ("First") );
-    m_ColTitles.Add (_ ("Second") );
-    m_ColTitles.Add (_ ("Third") );
-    m_ColTitles.Add (_ ("Fourth") );
-    m_ColTitles.Add (_ ("Fifth") );
+    m_ColTitles.Add(_("!Define Column Titles"));
+    m_ColTitles.Add(_("!Text lines that begin with \"!\" are ignored, but blank"));
+    m_ColTitles.Add(_("!lines are significant (a blank line is a blank title for"));
+    m_ColTitles.Add(_("!a column)."));
+    m_ColTitles.Add(_("First"));
+    m_ColTitles.Add(_("Second"));
+    m_ColTitles.Add(_("Third"));
+    m_ColTitles.Add(_("Fourth"));
+    m_ColTitles.Add(_("Fifth"));
 
-    m_TreeData.Add (_ ("! Define node data for a wxTreeCtrl.") );
-    m_TreeData.Add (_ ("! Note that all lines that start with a \"!\" are comment lines and ignored.") );
-    m_TreeData.Add (_ ("! Also, all empty lines are ignored (a line of spaces is not an empty line).") );
-    m_TreeData.Add (_ ("!") );
-    m_TreeData.Add (_ ("! The node data is organized similar to a Tabbed Hierarchical Values (THV) file,") );
-    m_TreeData.Add (_ ("! such that each line of text represents one label for a single tree node.") );
-    m_TreeData.Add (_ ("! The number of leading TAB characters indicate the indentation level of the") );
-    m_TreeData.Add (_ ("! tree node.") );
-    m_TreeData.Add (_ ("!") );
-    m_TreeData.Add (_ ("! Space (or blank) characters are not used the same as TAB characters; if your") );
-    m_TreeData.Add (_ ("! editor substitutes spaces for TABs, use the \"TAB Char\" attribute to change") );
-    m_TreeData.Add (_ ("! the character used to indicate the tree node level.") );
-    m_TreeData.Add (_ ("!") );
-    m_TreeData.Add (_ ("! Following is a sample tree data definition:") );
-    m_TreeData.Add (_ ("") );
-    m_TreeData.Add (_ ("First level, no tabs\tSecond Column\tThird Column\tFourth Column\tFifth Column") );
-    m_TreeData.Add (_ ("\tSecond level, 1 tab\tSecond Column\tThird Column\tFourth Column\tFifth Column") );
-    m_TreeData.Add (_ ("\t\tThird level, 2 tabs\tSecond Column\tThird Column\tFourth Column\tFifth Column") );
-    m_TreeData.Add (_ ("First level again, no tabs\tSecond Column\tThird Column\tFourth Column\tFifth Column") );
-    m_TreeData.Add (_ ("\tSecond level again, 1 tab\tSecond Column\tThird Column\tFourth Column\tFifth Column") );
-    m_TreeData.Add (_ ("\tAnother second level, 1 tab\tSecond Column\tThird Column\tFourth Column\tFifth Column") );
-    m_TreeData.Add (_ ("\t\tThird level again, 2 tabs\tSecond Column\tThird Column\tFourth Column\tFifth Column") );
-    m_TreeData.Add (_ ("") );
+    m_TreeData.Add(_("! Define node data for a wxTreeCtrl."));
+    m_TreeData.Add(_("! Note that all lines that start with a \"!\" are comment lines and ignored."));
+    m_TreeData.Add(_("! Also, all empty lines are ignored (a line of spaces is not an empty line)."));
+    m_TreeData.Add(_T("!"));
+    m_TreeData.Add(_("! The node data is organized similar to a Tabbed Hierarchical Values (THV) file,"));
+    m_TreeData.Add(_("! such that each line of text represents one label for a single tree node."));
+    m_TreeData.Add(_("! The number of leading TAB characters indicate the indentation level of the"));
+    m_TreeData.Add(_("! tree node."));
+    m_TreeData.Add(_T("!"));
+    m_TreeData.Add(_("! Space (or blank) characters are not used the same as TAB characters; if your"));
+    m_TreeData.Add(_("! editor substitutes spaces for TABs, use the \"TAB Char\" attribute to change"));
+    m_TreeData.Add(_("! the character used to indicate the tree node level."));
+    m_TreeData.Add(_T("!"));
+    m_TreeData.Add(_("! Following is a sample tree data definition:"));
+    m_TreeData.Add(_T(""));
+    m_TreeData.Add(_("First level, no tabs\tSecond Column\tThird Column\tFourth Column\tFifth Column"));
+    m_TreeData.Add(_("\tSecond level, 1 tab\tSecond Column\tThird Column\tFourth Column\tFifth Column"));
+    m_TreeData.Add(_"\t\tThird level, 2 tabs\tSecond Column\tThird Column\tFourth Column\tFifth Column"));
+    m_TreeData.Add(_("First level again, no tabs\tSecond Column\tThird Column\tFourth Column\tFifth Column"));
+    m_TreeData.Add(_("\tSecond level again, 1 tab\tSecond Column\tThird Column\tFourth Column\tFifth Column"));
+    m_TreeData.Add(_("\tAnother second level, 1 tab\tSecond Column\tThird Column\tFourth Column\tFifth Column"));
+    m_TreeData.Add(_("\t\tThird level again, 2 tabs\tSecond Column\tThird Column\tFourth Column\tFifth Column"));
+    m_TreeData.Add(_T(""));
 
 
 
@@ -160,15 +160,15 @@ void wxsTreeListCtrl::OnBuildCreatingCode()
 
     if (GetLanguage() == wxsCPP)
     {
-        AddHeader (_T ("<wx/treelistctrl.h>"),GetInfo().ClassName,0);
-        Codef (_T ("%C(%W, %I, %P, %S, %T, %V, %N);\n") );
+        AddHeader (_T("<wx/treelistctrl.h>"),GetInfo().ClassName,0);
+        Codef(_T("%C(%W, %I, %P, %S, %T, %V, %N);\n"));
         BuildItemCode();
         BuildSetupWindowCode();
     }
     else
     {
-        wxsCodeMarks::Unknown (_T ("wxsTreeCtrl::OnBuildCreatingCode"),GetLanguage() );
-    };
+        wxsCodeMarks::Unknown(_T("wxsTreeCtrl::OnBuildCreatingCode"),GetLanguage());
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -189,32 +189,32 @@ wxObject* wxsTreeListCtrl::OnBuildPreview (wxWindow* Parent,long Flags)
 void wxsTreeListCtrl::OnEnumWidgetProperties (long Flags)
 {
     static const long    TabValues[] = {        0,       1,       2,  0};
-    static const wxChar* TabNames[]  = {_T ("TAB"), _T ("+"), _T (">"),  0};
+    static const wxChar* TabNames[]  = {_T("TAB"), _T("+"), _T(">"),  0};
 
 
 // how many columns?
 
-    WXS_LONG (wxsTreeListCtrl, m_ColCount, _ ("Column Count"), _ ("colcount"), 1);
+    WXS_LONG (wxsTreeListCtrl, m_ColCount, _("Column Count"), _T("colcount"), 1);
 
 // default size of each column
 
-    WXS_LONG (wxsTreeListCtrl, m_ColWidth, _ ("Column Width"), _ ("colwidth"), 64);
+    WXS_LONG (wxsTreeListCtrl, m_ColWidth, _("Column Width"), _T("colwidth"), 64);
 
 // select the char to delimit the node levels
 
-    WXS_ENUM (wxsTreeListCtrl, m_TabChar, _ ("TAB Char"), _ ("tabchar"), TabValues, TabNames, 0);
+    WXS_ENUM (wxsTreeListCtrl, m_TabChar, _("TAB Char"), _T("tabchar"), TabValues, TabNames, 0);
 
 // pre-load column titles
 
-    WXS_ARRAYSTRING (wxsTreeListCtrl, m_ColTitles, _ ("Column Titles"), _ ("coltitles"), _ ("item") );
+    WXS_ARRAYSTRING (wxsTreeListCtrl, m_ColTitles, _("Column Titles"), _T("coltitles"), _T("item"));
 
 // allow user to pre-load tree node data
 
-    WXS_ARRAYSTRING (wxsTreeListCtrl, m_TreeData, _ ("Tree Items"), _ ("treedata"), _ ("node") );
+    WXS_ARRAYSTRING (wxsTreeListCtrl, m_TreeData, _("Tree Items"), _T("treedata"), _T("node"));
 
 // is this tree read-only?
 
-    WXS_BOOL (wxsTreeListCtrl, m_ReadOnly, _ ("Read Only"), _ ("readonly"), false);
+    WXS_BOOL (wxsTreeListCtrl, m_ReadOnly, _("Read Only"), _T("readonly"), false);
 
 }
 
@@ -232,23 +232,26 @@ void wxsTreeListCtrl::BuildItemCode (void)
 
 // enuough columns?
 
-    if (m_ColCount < 1) { m_ColCount = 1; }
+    if (m_ColCount < 1)
+        m_ColCount = 1;
 
 // add all the col info
 
     n = 0;
     for (i=0; i < (int)m_ColTitles.GetCount(); i++)
     {
-        if (n >= m_ColCount) { break; }
+        if (n >= m_ColCount)
+            break;
 
         ss = m_ColTitles.Item (i);
         ss.Trim (true);
         ss.Trim (false);
-        if (ss[0] == '!') { continue; }
+        if (ss[0] == '!')
+            continue;
 
-        Codef (_ ("%AAddColumn(%t, %d);\n"), ss.wx_str(), static_cast<int>(m_ColWidth));
+        Codef (_T("%AAddColumn(%t, %d);\n"), ss.wx_str(), static_cast<int>(m_ColWidth));
         n += 1;
-    };
+    }
 
 // count number of nodes, add 1 for the root
 
@@ -256,30 +259,34 @@ void wxsTreeListCtrl::BuildItemCode (void)
     for (i=0; i < (int)m_TreeData.GetCount(); i++)
     {
         ss = m_TreeData.Item (i);
-        if (ss.Len() == 0) { continue; }
-        if (ss[0] == '!') { continue; }
+        if (ss.Len() == 0)
+            continue;
+
+        if (ss[0] == '!')
+            continue;
 
         n += 1;
-    };
+    }
 
     n += 1;
 
 // name of the array of node IDs
 
-    idname = GetVarName() + _ ("ID");
+    idname = GetVarName() + _("ID");
 
 // add declaration for that many IDs
 
-    ss.Printf (_ ("wxTreeItemId    %s[%d];"), idname.wx_str(), n);
+    ss.Printf (_T("wxTreeItemId    %s[%d];"), idname.wx_str(), n);
     AddDeclaration (ss);
 
 // make sure there is no false readings
 
-    for (i=0; i < kLMAX; i++) { id[i] = 0; }
+    for (i = 0; i < kLMAX; i++)
+        id[i] = 0;
 
 // make our own root item
 
-    Codef (_ ("%s[0] = %AAddRoot(_(\"(root)\"));\n"), idname.wx_str() );
+    Codef (_T("%s[0] = %AAddRoot(_(\"(root)\"));\n"), idname.wx_str() );
     id[0] = 0;
 
 // and now each item in the tree data list
@@ -294,54 +301,62 @@ void wxsTreeListCtrl::BuildItemCode (void)
 
 // some lines are skipped over
 
-        if (ss.Len() == 0) { continue; }
-        if (ss[0] == '!') { continue; }
+        if (ss.Len() == 0)
+            continue;
+
+        if (ss[0] == '!')
+            continue;
 
 // get the level and the label texts
 
         ParseItems (ss, n, items);
-        if (n <= 0) { n = 1; }
+        if (n <= 0)
+            n = 1;
 
 // ID of the new item
 
         k += 1;
         id[n] = k;
-        ss.Printf (_ ("%s[%d]"), idname.c_str(), k);
+        ss.Printf (_T("%s[%d]"), idname.c_str(), k);
 
 // the parent item
 
         j = id[n-1];
-        pp.Printf (_ ("%s[%d]"), idname.c_str(), id[n-1]);
+        pp.Printf (_T("%s[%d]"), idname.c_str(), id[n-1]);
 
 // base text of the new item
 
-        if (items.GetCount() == 0) { tt = _ (""); }
-        else { tt = items.Item (0); }
+        if (items.GetCount() == 0)
+            tt = _T("");
+        else
+            tt = items.Item (0);
 
 // make the base item
 
-        Codef (_ ("%s = %AAppendItem(%s, _(\"%s\"));\n"), ss.wx_str(), pp.wx_str(), tt.wx_str() );
+        Codef (_T("%s = %AAppendItem(%s, _(\"%s\"));\n"), ss.wx_str(), pp.wx_str(), tt.wx_str() );
 
 // now each of the sub-items
 
         for (j=1; j < (int)items.GetCount(); j++)
         {
-            if (j >= m_ColCount) { break; }
+            if (j >= m_ColCount)
+                break;
+
             tt = items.Item (j);
-            Codef (_ ("%ASetItemText(%s, %d, %t);\n"), ss.wx_str(), j, tt.wx_str() );
-        };
-    };
+            Codef (_T("%ASetItemText(%s, %d, %t);\n"), ss.wx_str(), j, tt.wx_str() );
+        }
+    }
 
 // show everything
 
-    Codef (_ ("%AExpandAll(%s[0]);\n"), idname.wx_str() );
+    Codef (_T("%AExpandAll(%s[0]);\n"), idname.wx_str() );
 
 // read-only or editable?
 
     for (i=0; i<m_ColCount; i++)
     {
-        Codef (_ ("%ASetColumnEditable(%d, %b);\n"), i, m_ReadOnly);
-    };
+        Codef (_T("%ASetColumnEditable(%d, %b);\n"), i, m_ReadOnly);
+    }
 
 
 }
@@ -358,27 +373,31 @@ void wxsTreeListCtrl::PreviewItemCode (wxTreeListCtrl* inPreview)
 
 // enuough columns?
 
-    if (m_ColCount < 1) { m_ColCount = 1; }
+    if (m_ColCount < 1)
+        m_ColCount = 1;
 
 // add all the col info
 
     n = 0;
     for (i=0; i < (int)m_ColTitles.GetCount(); i++)
     {
-        if (n >= m_ColCount) { break; }
+        if (n >= m_ColCount)
+            break;
 
         ss = m_ColTitles.Item (i);
         ss.Trim (true);
         ss.Trim (false);
-        if (ss[0] == '!') { continue; }
+        if (ss[0] == '!')
+            continue;
 
         inPreview->AddColumn (ss, m_ColWidth);
         n += 1;
-    };
+    }
 
 // make sure there is no false readings
 
-    for (i=0; i < kLMAX; i++) { id[i].Unset(); }
+    for (i=0; i < kLMAX; i++)
+        id[i].Unset();
 
 // make our own root item
 
@@ -395,20 +414,27 @@ void wxsTreeListCtrl::PreviewItemCode (wxTreeListCtrl* inPreview)
 
 // some lines are skipped over
 
-        if (ss.Len() == 0) { continue; }
-        if (ss[0] == '!') { continue; }
+        if (ss.Len() == 0)
+            continue;
+
+        if (ss[0] == '!')
+            continue;
 
 // get the level and the label texts
 
         ParseItems (ss, n, items);
-        if (n <= 0) { n = 1; }
+        if (n <= 0)
+            n = 1;
 
 // make a new item, checking for valid parent
 
-        if (! id[n-1].IsOk() ) { return; }
+        if (! id[n-1].IsOk() )
+            return;
 
-        if (items.GetCount() == 0) { tt = _ (""); }
-        else { tt = items.Item (0); }
+        if (items.GetCount() == 0)
+            tt = _T("");
+        else
+            tt = items.Item(0);
 
         id[n] = inPreview->AppendItem (id[n-1], tt);
 
@@ -416,11 +442,13 @@ void wxsTreeListCtrl::PreviewItemCode (wxTreeListCtrl* inPreview)
 
         for (j=1; j < (int)items.GetCount(); j++)
         {
-            if (j >= m_ColCount) { break; }
+            if (j >= m_ColCount)
+                break;
+
             tt = items.Item (j);
             inPreview->SetItemText (id[n], j, tt);
-        };
-    };
+        }
+    }
 
 // show everything
 
@@ -431,7 +459,7 @@ void wxsTreeListCtrl::PreviewItemCode (wxTreeListCtrl* inPreview)
     for (i=0; i<m_ColCount; i++)
     {
         inPreview->SetColumnEditable (i, m_ReadOnly);
-    };
+    }
 
 }
 
@@ -448,7 +476,7 @@ void wxsTreeListCtrl::ParseItems (wxString inSource, int& outLevel, wxArrayStrin
     if      (m_TabChar == 0) { tab = '\t'; }
     else if (m_TabChar == 1) { tab = '+'; }
     else if (m_TabChar == 2) { tab = '>'; }
-    else { tab = '\t'; }
+    else                     { tab = '\t'; }
 
 // working copy
 
@@ -465,7 +493,8 @@ void wxsTreeListCtrl::ParseItems (wxString inSource, int& outLevel, wxArrayStrin
     {
         n += 1;
         ss.erase (0, 1);
-    };
+    }
+    
     outLevel = n + 1;
 
 // copy over labels
@@ -473,11 +502,14 @@ void wxsTreeListCtrl::ParseItems (wxString inSource, int& outLevel, wxArrayStrin
     i = 0;
     while (i < (int)ss.Len() )
     {
-        tt = _ ("");
-        while ( (i < (int)ss.Len() ) && (ss[i] != tab) ) { tt += ss[i++]; }
+        tt = _T("");
+        while ( (i < (int)ss.Len() ) && (ss[i] != tab) )
+            tt += ss[i++];
+
         outItems.Add (tt);
-        if (i < (int)ss.Len() ) { i += 1; }
-    };
+        if (i < (int)ss.Len() )
+            i += 1;
+    }
 
 // and the last item
 
@@ -486,10 +518,3 @@ void wxsTreeListCtrl::ParseItems (wxString inSource, int& outLevel, wxArrayStrin
     tt.Trim (false);
     outItems.Add (tt);
 }
-
-
-
-
-
-
-
