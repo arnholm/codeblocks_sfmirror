a808e5d14ac27c072e5cc166e30b9fd86467242f
wh11204 <wh11204@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2022-11-24 13:05:17 +0000

    - Fix possible out-of-bounds array access (ticket #1334). wxScintilla is an external library, but they has not fixed this yet and it may crash C::B.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13063 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/wxscintilla/src/scintilla/lexers/LexMetapost.cxx
+++ src/sdk/wxscintilla/src/scintilla/lexers/LexMetapost.cxx
@@ -179,7 +179,8 @@ static void ColouriseMETAPOSTDoc(
 	}
 
 	WordList &keywords  = *keywordlists[0] ;
-	WordList &keywords2 = *keywordlists[extraInterface-1] ;
+	WordList emptyList;
+	WordList &keywords2 = extraInterface ? *keywordlists[extraInterface-1] : emptyList;
 
 	StyleContext sc(startPos, length, SCE_METAPOST_TEXT, styler) ;
 
