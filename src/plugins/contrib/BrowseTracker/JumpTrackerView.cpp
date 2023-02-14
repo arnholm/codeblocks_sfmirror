87523b11f8d4bafbd767a673f610c3597e338aac
pecanh <pecanh@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2023-02-14 19:04:18 +0000

    - BrowseTracker - commit missing files after forgetting to manually add them via tortoiseSVN
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13212 2a5c6006-c6dd-42ca-98ab-0921f2732cef

---
+++ src/plugins/contrib/BrowseTracker/JumpTrackerView.cpp
@@ -0,0 +1,167 @@
+/*
+ * This file is part of the Code::Blocks IDE and licensed under the GNU Lesser General Public License, version 3
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * $Revision: 63 $
+ * $Id: lspdiagresultslog.cpp 63 2022-05-20 17:21:14Z pecanh $
+ * $HeadURL: https://svn.code.sf.net/p/cb-clangd-client/code/trunk/clangd_client/src/LSPclient/lspdiagresultslog.cpp $
+ */
+
+// A new line to force commit to include this file //(ph 2023/02/14)
+
+//#include "sdk_precomp.h" gets not used because `EXPORT_LIB' not defined [-Winvalid-pch] error
+#include "sdk.h"
+
+#ifndef CB_PRECOMP
+    #include <wx/arrstr.h>
+    #include <wx/filename.h>
+    #include <wx/listctrl.h>
+    #include "manager.h"
+    #include "editormanager.h"
+    #include "cbeditor.h"
+#endif
+#include <wx/menu.h>
+#include <wx/checklst.h>
+#include "wx/xrc/xmlres.h"
+
+#include <multiselectdlg.h>
+#include <annoyingdialog.h>
+#include <configmanager.h>
+
+#include "cbstyledtextctrl.h"
+#include "JumpTrackerView.h"
+
+namespace
+{
+}
+
+BEGIN_EVENT_TABLE(JumpTrackerView, wxEvtHandler)
+//
+END_EVENT_TABLE()
+
+// ----------------------------------------------------------------------------
+JumpTrackerView::JumpTrackerView(const wxArrayString& titles_in, wxArrayInt& widths_in)
+// ----------------------------------------------------------------------------
+    : ListCtrlLogger(titles_in, widths_in)
+
+{
+    m_ID_List = wxNewId();
+    m_pListCtrl =  dynamic_cast<wxListCtrl*>(CreateControl(Manager::Get()->GetAppFrame()));
+
+}
+// ----------------------------------------------------------------------------
+wxEvtHandler* JumpTrackerView::FindEventHandler(wxEvtHandler* pEvtHdlr)
+// ----------------------------------------------------------------------------
+{
+    wxEvtHandler* pFoundEvtHdlr =  Manager::Get()->GetAppWindow()->GetEventHandler();
+
+    while (pFoundEvtHdlr != nullptr)
+    {
+        if (pFoundEvtHdlr == pEvtHdlr)
+            return pFoundEvtHdlr;
+        pFoundEvtHdlr = pFoundEvtHdlr->GetNextHandler();
+    }
+    return nullptr;
+}
+// ----------------------------------------------------------------------------
+JumpTrackerView::~JumpTrackerView()
+// ----------------------------------------------------------------------------
+{
+    //dtor
+    if (FindEventHandler(this))
+        Manager::Get()->GetAppWindow()->RemoveEventHandler(this);   //(ph 2021/04/17)
+}
+// ----------------------------------------------------------------------------
+wxWindow* JumpTrackerView::CreateControl(wxWindow* parent)
+// ----------------------------------------------------------------------------
+{
+    ListCtrlLogger::CreateControl(parent);
+    control->SetId(m_ID_List);
+    Connect(m_ID_List, -1, wxEVT_COMMAND_LIST_ITEM_ACTIVATED,
+            (wxObjectEventFunction) (wxEventFunction) (wxCommandEventFunction)
+            &JumpTrackerView::OnDoubleClick);
+    Manager::Get()->GetAppWindow()->PushEventHandler(this);
+    m_pControl = control;
+
+    return m_pControl;
+}
+// ----------------------------------------------------------------------------
+bool JumpTrackerView::HasFeature(Feature::Enum feature) const
+// ----------------------------------------------------------------------------
+{
+    if (feature == Feature::Additional)
+        return true;
+    else
+        return ListCtrlLogger::HasFeature(feature);
+}
+
+// ----------------------------------------------------------------------------
+void JumpTrackerView::AppendAdditionalMenuItems(wxMenu &menu)
+// ----------------------------------------------------------------------------
+{
+}
+
+// ----------------------------------------------------------------------------
+void JumpTrackerView::FocusEntry(size_t index)
+// ----------------------------------------------------------------------------
+{
+    if (not control->GetItemCount()) return;
+    if (index < (size_t)control->GetItemCount())
+    {
+        control->SetItemState(index, wxLIST_STATE_FOCUSED | wxLIST_STATE_SELECTED, wxLIST_STATE_FOCUSED | wxLIST_STATE_SELECTED);
+        control->EnsureVisible(index);
+    }
+}
+
+// ----------------------------------------------------------------------------
+void JumpTrackerView::SyncEditor(int selIndex)
+// ----------------------------------------------------------------------------
+{
+    wxFileName filename(control->GetItemText(selIndex));
+    wxString file;
+    if (not filename.Exists()) return;
+    if (!filename.IsAbsolute())
+        filename.MakeAbsolute(m_Base);
+    file = filename.GetFullPath();
+
+    wxListItem li;
+    li.m_itemId = selIndex;
+    li.m_col = 1;
+    li.m_mask = wxLIST_MASK_TEXT;
+    control->GetItem(li);
+    long line = 0;
+    li.m_text.ToLong(&line);
+    cbEditor* ed = Manager::Get()->GetEditorManager()->Open(file);
+    if (!line || !ed)
+        return;
+
+    line -= 1;
+    ed->Activate();
+    ed->GotoLine(line);
+
+    if (cbStyledTextCtrl* ctrl = ed->GetControl()) {
+        ctrl->EnsureVisible(line);
+    }
+}
+
+// ----------------------------------------------------------------------------
+void JumpTrackerView::OnDoubleClick(cb_unused wxCommandEvent& event)
+// ----------------------------------------------------------------------------
+{
+    // go to the relevant file/line
+    if (control->GetSelectedItemCount() == 0)
+        return;
+
+    // find selected item index
+    int index = control->GetNextItem(-1,
+                                     wxLIST_NEXT_ALL,
+                                     wxLIST_STATE_SELECTED);
+    m_lastDoubleClickIndex = index;
+
+    m_bJumpInProgress = true;
+    SyncEditor(index);
+    FocusEntry(index);
+    m_bJumpInProgress = false;
+    return;
+
+} // end of OnDoubleClick
---
+++ src/plugins/contrib/BrowseTracker/JumpTrackerView.h
@@ -0,0 +1,47 @@
+#ifndef JUMPTRACKERVIEW_H_INCLUDED
+#define JUMPTRACKERVIEW_H_INCLUDED
+
+/*
+ * This file is part of the Code::Blocks IDE and licensed under the GNU Lesser General Public License, version 3
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ */
+
+// A new line to force svn to commit this file.
+
+#include <wx/string.h>
+#include <wx/panel.h>
+
+#include "loggers.h"
+
+class wxArrayString;
+class wxCommandEvent;
+
+class JumpTrackerView : public ListCtrlLogger, public wxEvtHandler
+{
+	public:
+		JumpTrackerView(const wxArrayString& titles, wxArrayInt& widths);
+		~JumpTrackerView() override;
+		void FocusEntry(size_t index);
+		void SetBasePath(const wxString base){ m_Base = base; }
+
+		wxWindow* CreateControl(wxWindow* parent) override;
+        bool      HasFeature(Feature::Enum feature) const override;
+        void      AppendAdditionalMenuItems(wxMenu &menu) override;
+
+        int m_ID_List = wxNewId();
+        wxListCtrl* m_pListCtrl = nullptr;
+        wxListCtrl* m_pControl = nullptr;
+        int         m_lastDoubleClickIndex = 0;
+        bool        m_bJumpInProgress = false;
+
+	protected:
+        void OnDoubleClick(wxCommandEvent& event);
+        void SyncEditor(int selIndex);
+        wxEvtHandler* FindEventHandler(wxEvtHandler* pEvtHdlr);
+
+        wxString m_Base;
+	private:
+        DECLARE_EVENT_TABLE()
+};
+
+#endif // JUMPTRACKERVIEW_H_INCLUDED
