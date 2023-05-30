8f35a71b116ab1156c565b90d887821f90735178
mortenmacfly <mortenmacfly@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2023-05-30 16:05:41 +0000

    - correct version for wxSmithDemo
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13301 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/wxSmithDemo/version.h
+++ src/plugins/contrib/wxSmithDemo/version.h
@@ -2,32 +2,32 @@
 #define VERSION_H
 
 namespace AutoVersion{
-	
+
 	//Date Version Types
-	static const char DATE[] = "09";
-	static const char MONTH[] = "10";
-	static const char YEAR[] = "2011";
-	static const char UBUNTU_VERSION_STYLE[] = "11.10";
-	
+	static const char DATE[] = "21";
+	static const char MONTH[] = "08";
+	static const char YEAR[] = "2020";
+	static const char UBUNTU_VERSION_STYLE[] =  "20.08";
+
 	//Software Status
-	static const char STATUS[] = "Alpha";
-	static const char STATUS_SHORT[] = "a";
-	
+	static const char STATUS[] =  "Alpha";
+	static const char STATUS_SHORT[] =  "a";
+
 	//Standard Version Type
-	static const long MAJOR = 1;
-	static const long MINOR = 0;
-	static const long BUILD = 5;
-	static const long REVISION = 19;
-	
+	static const long MAJOR  = 1;
+	static const long MINOR  = 0;
+	static const long BUILD  = 7;
+	static const long REVISION  = 32;
+
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 13;
-	#define RC_FILEVERSION 1,0,5,19
-	#define RC_FILEVERSION_STRING "1, 0, 5, 19\0"
-	static const char FULLVERSION_STRING[] = "1.0.5.19";
-	
+	static const long BUILDS_COUNT  = 16;
+	#define RC_FILEVERSION 1,0,7,32
+	#define RC_FILEVERSION_STRING "1, 0, 7, 32\0"
+	static const char FULLVERSION_STRING [] = "1.0.7.32";
+
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 5;
-	
+	static const long BUILD_HISTORY  = 7;
+
 
 }
 #endif //VERSION_H
