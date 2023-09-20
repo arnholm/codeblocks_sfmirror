14d6d19992176c6c970f8bdb38123aaf60a3fc9f
wh11204 <wh11204@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2023-09-20 14:00:57 +0000

    - AStyle: Initialize member (ticket #1416, thanks Martin Strunz). Fix indentation.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13359 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/astyle/asstreamiterator.cpp
+++ src/plugins/astyle/asstreamiterator.cpp
@@ -15,13 +15,14 @@
 #include "globals.h"
 
 ASStreamIterator::ASStreamIterator(cbEditor* cbe, const wxChar* in) :
-  m_Ed(cbe),
-  m_CharPtr(in),
-  m_SavedCharPtr(0),
-  m_CurChar(0),
-  m_CurLine(0),
-  m_FoundBookmark(false),
-  m_FoundBreakpoint(false)
+    m_Ed(cbe),
+    m_CharPtr(in),
+    m_SavedCharPtr(0),
+    m_CurChar(0),
+    m_SavedCurChar(0),
+    m_CurLine(0),
+    m_FoundBookmark(false),
+    m_FoundBreakpoint(false)
 {
 }
 
