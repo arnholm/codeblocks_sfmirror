00e32959b623a2df42caf09c122aa11c7a93c722
pecanh <pecanh@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2024-03-07 04:06:34 +0000

    - CodeCompletion: Restore CCLogger to rev 13427
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13486 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/codecompletion/parser/cclogger.cpp
+++ src/plugins/codecompletion/parser/cclogger.cpp
@@ -15,8 +15,6 @@
 
 #include <logmanager.h> // F()
 #include <globals.h>    // cbC2U for cbAssert macro
-#include <configmanager.h>
-#include <pluginmanager.h>
 
 std::unique_ptr<CCLogger> CCLogger::s_Inst;
 
@@ -25,14 +23,7 @@ bool           g_EnableDebugTrace     = false;
 const wxString g_DebugTraceFile       = wxEmptyString;
 long           g_idCCAddToken         = wxNewId();
 long           g_idCCLogger           = wxNewId();
-long           g_idCCErrorLogger      = wxNewId();
 long           g_idCCDebugLogger      = wxNewId();
-long           g_idCCDebugErrorLogger = wxNewId();
-
-#define CC_GLOBAL_DEBUG_OUTPUT 0 //(ph 2024/01/29)
-// I cannot get CC_GLOBAL_DEBUG_OUTPUT set to 0.
-// It screws up pch when defined in the project settings
-// and does not work when set locally here.
 
 // Set CC_GLOBAL_DEBUG_OUTPUT via #define in the project options to 0:
 // --> No debugging output for CC will be generated (this is the default)
@@ -42,22 +33,17 @@ long           g_idCCDebugErrorLogger = wxNewId();
 // --> Debugging output for CC will be generated only, when the user enabled this
 //     through the menu in the symbols browser (similar to debug smart sense)
 // For single files only, the same applies to the individual #define per file
-// (like CC_BUILDERTHREAD_DEBUG_OUTPUT, CC_PARSEMANAGER_DEBUG_OUTPUT, etc.)
+// (like CC_BUILDERTHREAD_DEBUG_OUTPUT, CC_NATIVEPARSER_DEBUG_OUTPUT, etc.)
 
-// ----------------------------------------------------------------------------
 CCLogger::CCLogger() :
-    // ----------------------------------------------------------------------------
     m_Parent(nullptr),
     m_LogId(-1),
-    m_LogErrorId(-1),
     m_DebugLogId(-1),
     m_AddTokenId(-1)
 {
 }
 
-// ----------------------------------------------------------------------------
 /*static*/ CCLogger* CCLogger::Get()
-// ----------------------------------------------------------------------------
 {
     if (!s_Inst.get())
         s_Inst.reset(new CCLogger);
@@ -66,32 +52,15 @@ CCLogger::CCLogger() :
 }
 
 // Initialized from CodeCompletion constructor
-// ----------------------------------------------------------------------------
-//-void CCLogger::Init(wxEvtHandler* parent, int logId, int debugLogId, int debugLogErrorId, int addTokenId)
-void CCLogger::Init(wxEvtHandler* parent, int logId, int logErrorId, int debugLogId, int debugLogErrorId, int addTokenId)
-// ----------------------------------------------------------------------------
+void CCLogger::Init(wxEvtHandler* parent, int logId, int debugLogId, int addTokenId)
 {
     m_Parent     = parent;
     m_LogId      = logId;
     m_DebugLogId = debugLogId;
-    m_DebugLogErrorId = debugLogErrorId;
     m_AddTokenId = addTokenId;
-    m_AddTokenId = addTokenId;
-
-    m_pCfgMgr = nullptr;
-    wxString configName;
-    #if defined (BUILDING_PLUGIN)
-        PluginInfo* pInfo = (PluginInfo*)(Manager::Get()->GetPluginManager()->GetPluginInfo((cbPlugin*)parent));
-        if (pInfo and (pInfo->name == "CodeCompletion" )) configName = "code_completion";
-        else if (pInfo and (pInfo->name == "clangd_client")) configName = "clangd_client";
-        if (pInfo and configName.Length())
-            m_pCfgMgr = Manager::Get()->GetConfigManager(configName);
-    #endif
 }
 
-// ----------------------------------------------------------------------------
 void CCLogger::AddToken(const wxString& msg)
-// ----------------------------------------------------------------------------
 {
     if (!m_Parent || m_AddTokenId<1) return;
 
@@ -104,9 +73,7 @@ void CCLogger::AddToken(const wxString& msg)
 #endif
 }
 
-// ----------------------------------------------------------------------------
-void CCLogger::Log(const wxString& msg, int id)
-// ----------------------------------------------------------------------------
+void CCLogger::Log(const wxString& msg)
 {
     //Could crash here; should check if shutting down
     if (Manager::IsAppShuttingDown())
@@ -123,9 +90,7 @@ void CCLogger::Log(const wxString& msg, int id)
 #endif
 }
 
-// ----------------------------------------------------------------------------
-void CCLogger::DebugLog(const wxString& msg, int id)
-// ----------------------------------------------------------------------------
+void CCLogger::DebugLog(const wxString& msg)
 {
     // Could crash here; should check if shutting down
     if (Manager::IsAppShuttingDown())
@@ -133,29 +98,11 @@ void CCLogger::DebugLog(const wxString& msg, int id)
 
     if (!m_Parent || m_DebugLogId<1) return;
 
-    bool debugLogging = false;
-    if (m_pCfgMgr) debugLogging = m_pCfgMgr->ReadBool("/logPluginDebug_check", false);
-    if ((not debugLogging) and (id == m_DebugLogId)) return;
-
-    // Always allow debugError log messages
-    CodeBlocksThreadEvent evt(wxEVT_COMMAND_MENU_SELECTED, id);
+    CodeBlocksThreadEvent evt(wxEVT_COMMAND_MENU_SELECTED, m_DebugLogId);
     evt.SetString(msg);
-
-    #if CC_PROCESS_LOG_EVENT_TO_PARENT
+#if CC_PROCESS_LOG_EVENT_TO_PARENT
     m_Parent->ProcessEvent(evt);
-    #else
+#else
     wxPostEvent(m_Parent, evt);
-    #endif
-}
-// ----------------------------------------------------------------------------
-void CCLogger::LogError(const wxString& msg)
-// ----------------------------------------------------------------------------
-{
-    Log(msg, m_LogErrorId);
-}
-// ----------------------------------------------------------------------------
-void CCLogger::DebugLogError(const wxString& msg)
-// ----------------------------------------------------------------------------
-{
-    DebugLog(msg, m_DebugLogErrorId);
+#endif
 }
