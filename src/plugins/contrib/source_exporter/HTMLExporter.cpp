2d385ac4473be6362bcc589a07b98620c861e29b
wh11204 <wh11204@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2025-10-08 13:03:16 +0000

    * SourceExporter: Replace ISO8859-1 encoding with UTF-8 when generating HTML (thanks srm2000). wxStyledTextCtrl uses UTF-8 internally.
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13743 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/source_exporter/HTMLExporter.cpp
+++ src/plugins/contrib/source_exporter/HTMLExporter.cpp
@@ -55,13 +55,13 @@ namespace
 };
 
 const char *HTMLExporter::HTMLHeaderBEG =
-  "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n"
+  "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
   "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"
   "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
   "<head>\n";
 
 const char *HTMLExporter::HTMLMeta =
-  "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />\n"
+  "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n"
   "<meta name=\"generator\" content=\"Code::Blocks Exporter plugin\" />\n";
 
 const char *HTMLExporter::HTMLStyleBEG =
